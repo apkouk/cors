4d6f75dce9b50aca3e1ded6394e31a3d41b70d60
apkouk <cescorh@gmail.com>
2019-05-12 14:54:24 +0200

    First Commit

---
+++ Cors/Controllers/EmployeesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using Cors.Models;
+
+namespace Cors.Controllers
+{
+
+    public class EmployeesController : ApiController
+    {
+
+        public HttpResponseMessage Get(int id)
+        {
+            var employee = new Employee()
+            {
+                Id = id,
+                Name = "John Q Human",
+                Department = "Enforcement"
+            };
+
+
+            var response = Request.CreateResponse<Employee>(HttpStatusCode.OK, employee);
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
+            return response;
+        }
+
+        public HttpResponseMessage Put(Employee employee)
+        {
+            // Update logic goes here
+
+            var response = Request.CreateResponse<Employee>(HttpStatusCode.OK, employee);
+            //response.Headers.Add("Access-Control-Allow-Origin", "*");
+            return response;
+        }
+
+        //public HttpResponseMessage Options()
+        //{
+        //    //var response = new HttpResponseMessage();
+        //    //response.StatusCode = HttpStatusCode.OK;
+        //    //response.Headers.Add("Access-Control-Allow-Origin", "*");
+        //    //response.Headers.Add("Access-Control-Allow-Methods", "PUT");
+        //    //return response;
+        //}
+    }
+}
---
+++ Cors/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cors.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+    }
+}
